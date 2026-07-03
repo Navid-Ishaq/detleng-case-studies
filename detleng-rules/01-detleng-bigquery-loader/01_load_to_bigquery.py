875abac3804cd34044d5777dd5c5df2e234ad954
🎓 Navid Ishaq <mnvid@hotmail.com>
2026-07-03 11:04:35 +0200

    Update 01_load_to_bigquery.py

--- detleng-rules/01-detleng-bigquery-loader/01_load_to_bigquery.py
+++ detleng-rules/01-detleng-bigquery-loader/01_load_to_bigquery.py
@@ -1,1 +1,15 @@
+def run_pipeline():
 
+    load_config()
+
+    validate_schema()
+
+    create_dataset()
+
+    create_table()
+
+    load_data()
+
+    write_log()
+
+    print("Pipeline Completed")
